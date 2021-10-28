115db34fea232c60d0cb056202a4621e88ce956f
varunbkk <varun.prak@gmail.com>
2021-10-28 16:27:43 +0800

    Updated urls.py

---
+++ mysite/bookmyslot/urls.py
@@ -0,0 +1,16 @@
+from . import views
+from django.urls import path
+
+app_name = 'bookmyslot'
+
+urlpatterns = [
+    path('mybookings/',views.BookingList.as_view(),name='list'),
+    path('mybookings/<int:pk>/',views.BookingDetail.as_view(),name='detail'),
+    path('update/<int:pk>/',views.BookingUpdate.as_view(),name='update'),
+    path('delete/<int:pk>/',views.BookingDelete.as_view(),name='delete'),
+    path('new/',views.BookingCreate.as_view(),name='create'),
+    path('',views.HomeView.as_view(),name='home'),
+    path('about/',views.AboutView.as_view(),name='about'),
+    path('search/',views.search_bookings, name='search_bookings')
+
+]
