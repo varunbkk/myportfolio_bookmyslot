89054929c35999f88ee8f93fd1bc725abc907414
varunbkk <varun.prak@gmail.com>
2021-10-29 12:35:40 +0800

    Updated urls.py - changed search to function-based-view

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('',include('bookmyslot.urls')),
     path('accounts/login/',auth_views.LoginView.as_view(),name='login'),
     path('accounts/logout/',auth_views.LogoutView.as_view(),name='logout'),
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls)
 ]
