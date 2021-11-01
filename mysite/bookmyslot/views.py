a57f41ebe8230ef74354d250b92c1e525ef570f7
varunbkk <varun.prak@gmail.com>
2021-11-01 11:48:11 +0800

    Updated views.py with comments

--- mysite/bookmyslot/views.py
+++ mysite/bookmyslot/views.py
@@ -25,11 +25,18 @@ class BookingCreate(LoginRequiredMixin,CreateView):
     model = Booking
     form_class = BookingForm
 
+    # This function returns a dictionary that is passed to the __init__ portion of our Booking form
+    # We need to pass the current username to the form so that only the customers belonging to that specific username
+    # can be visible in the drop-down list
+
     def get_form_kwargs(self, **kwargs):
         form_kwargs = super(BookingCreate,self).get_form_kwargs(**kwargs)
         form_kwargs['username'] = self.request.user
         return form_kwargs
 
+    # This function validates the form before the form data is written to the database/model
+    # The form data is only stored in the memory (with the commit=False statement)
+    # It also insures that the user creating the form is the same as the currently logged in user
     def form_valid(self, form):
         self.object = form.save(commit=False)
         self.object.username = self.request.user
@@ -39,13 +46,14 @@ class BookingCreate(LoginRequiredMixin,CreateView):
 class BookingList(LoginRequiredMixin,ListView):
     login_url = '/login'
     model = Booking
+    # We paginate the list of bookings and limit 3 booking per each page
     paginate_by = 3
     template_name = 'bookmyslot/booking_list.html'
     context_object_name = 'bookings'
 
     def get_queryset(self):
         if not self.request.user.is_staff:
-            return Booking.objects.filter(username=self.request.user).exclude(delivery_details__date=dt.datetime.now(),delivery_details__time__lt=dt.datetime.now()).order_by('delivery_details')
+            return Booking.objects.filter(username=self.request.user).exclude(delivery_details__date__lte=dt.date.today()).order_by('delivery_details')
         else:
             return Booking.objects.all()
 
@@ -66,6 +74,7 @@ class BookingUpdate(LoginRequiredMixin,UpdateView):
         model = Booking
         form_class = BookingForm
 
+        # Identical to what we did in the CreateView class
         def get_form_kwargs(self, **kwargs):
             form_kwargs = super(BookingUpdate,self).get_form_kwargs(**kwargs)
             form_kwargs['username'] = self.request.user
@@ -82,12 +91,14 @@ class BookingDelete(LoginRequiredMixin,DeleteView):
     model = Booking
     success_url = reverse_lazy('bookmyslot:list')
 
+# We're using a function-based view to get search results for bookings, based on the keywords that a user enters
+# The user can search by delivery date or the booking number
 @login_required
 def search(request):
     query = request.GET.get('q')
 
     if query:
-        object_list = Booking.objects.filter(username=request.user).filter(Q(booking_number__icontains=query) | Q(delivery_details__date__icontains=query)).exclude(delivery_details__date=dt.datetime.now(),delivery_details__time__lt=dt.datetime.now()).order_by('delivery_details')
+        object_list = Booking.objects.filter(username=request.user).filter(Q(booking_number__icontains=query) | Q(delivery_details__date__icontains=query)).exclude(delivery_details__date__lte=dt.date.today()).order_by('delivery_details')
 
         result = object_list
     else:
