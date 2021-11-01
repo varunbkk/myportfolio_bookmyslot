7da7ba2d573c1348961fe5c6cd1bdb217b55b1be
varunbkk <varun.prak@gmail.com>
2021-11-01 11:47:53 +0800

    Updated models.py with comments

--- mysite/bookmyslot/models.py
+++ mysite/bookmyslot/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator,MaxValueValidator
 
 class Customer(models.Model):
+    # username is defined as FK to Django's User model, because a single user can have multiple customers
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     name = models.CharField(max_length=20,null=True)
 
@@ -13,6 +14,7 @@ class Customer(models.Model):
 
 # Create your models here.
 class Booking(models.Model):
+    # Similarly, customer_name is defined as FK to the Customer model, because a single customer can have multiple bookings
     customer_name = models.ForeignKey(Customer,on_delete=models.CASCADE,null=True)
     username = models.ForeignKey(User,on_delete=models.CASCADE)
     qty_plts = models.PositiveSmallIntegerField(default=1, validators=[MinValueValidator(1), MaxValueValidator(50)])
@@ -24,10 +26,14 @@ class Booking(models.Model):
     def __str__(self):
         return self.booking_number
 
+    # We use the save() function to concatenate the delivery date and time components of the delivery_details field
+    # into the booking_number field
     def save(self, **kwargs):
 
         self.booking_number = f"{self.delivery_details:%Y%m%d%H%M}"
         super().save(**kwargs)
 
+    # this provides the url for viewing an instance of the model object, once it is creatd.
+    # Instance of an object means a specific booking, which is denoted by a unique primary key(pk)
     def get_absolute_url(self):
         return reverse('bookmyslot:detail',kwargs={'pk':self.pk})
